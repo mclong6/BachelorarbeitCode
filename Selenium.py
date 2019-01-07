b5f9ed246118b874b5c05800af9d385285459b22
Marco Lang <marcolang95@web.de>
2019-01-07 18:13:24 +0100

    Refactor of Selenium.py

--- Selenium.py
+++ Selenium.py
@@ -6,24 +6,53 @@ from selenium import webdriver
 # ReactJS,VueJS) website, Selenium can help you;If you need to auto login, it depends the web page needs to execute JS
 # or not.
 
-url = "/profile/Wolfgang_Zeilmann"
+url_german_companies = "https://www.xing.com/search/companies?advanced_form=true&nrs=1&" \
+      "section=companies&filters%5Bcountry%5D%5B%5D=2921044"
 browser = webdriver.Chrome('/home/marco/Downloads/chromedriver')
-browser.get("https://login.xing.com/login")
 browser.implicitly_wait(10)
-browser.find_element_by_name("login_form[username]").send_keys("bachelorarbeit2@gmx.de")
-browser.find_element_by_name("login_form[password]").send_keys("bachelorarbeit2")
-browser.find_element_by_name("button").click()
-
 browser.implicitly_wait(10)
 browser.maximize_window()
-browser.find_element_by_name("keywords").send_keys("Wolfgang Zeilmann\n")
-html_of_search = browser.page_source
-soup = BeautifulSoup(html_of_search, "html.parser")
 
-print(soup.prettify())
+page_links_list = []
+companies_list = []
 
-all_links = soup.find_all("a", attrs={'href': re.compile("^(/profile/).*_.*/.*")})
-for link in all_links:
+username = "bachelorarbeit2@gmx.de"
+password = "bachelorarbeit2"
+
+
+def login_xing(username, password):
+    browser.get("https://login.xing.com/login")
+    browser.find_element_by_name("login_form[username]").send_keys(username)
+    browser.find_element_by_name("login_form[password]").send_keys(password)
+    browser.find_element_by_name("button").click()
+
+
+def open_link(url):
+    browser.get(url)
+    html_of_search = browser.page_source
+    html = BeautifulSoup(html_of_search, "html.parser")
+    return html
+
+
+def get_companies_links(html):
+    page_links = html.find_all("a", attrs={'href': re.compile("^(/search/companies).*")})
+
+    for link in page_links:
+        if not link.attrs["href"] in page_links_list:
+            companies_list.append(link.attrs['href'])
+
+    print(len(companies_list))
+    companies_links = html.find_all("a", attrs={'href': re.compile("^(https://www.xing.com/companies/|"
+                                                                   "https://www.xing.com/company/).*")})
+
     print('------------------------------------------------------')
-    print(link.attrs['href'])
-    browser.get('https://www.xing.com'+link.attrs['href'])
+
+    for link in companies_links:
+        if not link.attrs["href"] in companies_list:
+            companies_list.append(link.attrs['href'])
+
+    print(companies_list)
+
+
+login_xing(username,password)
+get_companies_links(open_link(url_german_companies))
