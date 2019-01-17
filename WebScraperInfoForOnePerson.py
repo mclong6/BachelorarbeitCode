687346aa4b8004ed34bfadb0731323b6a62875de
Marco Lang <marcolang95@web.de>
2019-01-17 14:30:34 +0100

    add search instag, facebook, linkedin

--- WebScraperInfoForOnePerson.py
+++ WebScraperInfoForOnePerson.py
@@ -14,7 +14,8 @@ facebook_login_page = "https://www.facebook.com/login.php"
 instagram_login_page = "https://www.instagram.com/accounts/login/?next=%2Fbachelor_arbeit2%2F&source=desktop_nav"
 xing_login_page = "https://login.xing.com/login"
 linkedin_login_page = "https://www.linkedin.com/uas/login"
-
+browser = webdriver.Chrome('/home/marco/Downloads/chromedriver')
+browser.implicitly_wait(10)
 
 class Person(object):
     def __init__(self, first_name, second_name, location, year_of_birth, instagram_name, facebook_name, company):
@@ -27,38 +28,7 @@ class Person(object):
         self.company = company
 
 
-def get_person_information():
-    first_name = input("Vorname: ")
-    second_name = input("Nachname: ")
-    location = input("Ort: ")
-    year_of_birth = input("Geburtsjahr: ")
-    instagram_name = input("Instagram-Name: ")
-    facebook_name = input("Facebook-Name: ")
-    company = input("Firma: ")
-    person_object = Person(first_name, second_name, location, year_of_birth, "michi0595", facebook_name, company)
-
-    if person_object.instagram_name is not "":
-        print(person_object.instagram_name)
-        login_to_any_page(facebook_login_page)
-
-
-def login_to_any_page(url):
-    browser = webdriver.Chrome('/home/marco/Downloads/chromedriver')
-    browser.implicitly_wait(10)
-    browser.maximize_window()
-    browser.get(url)
-
-    html_of_search = browser.page_source
-    html = BeautifulSoup(html_of_search, "html.parser")
-    time.sleep(1)
-    input_username = html.find("input", attrs={'type': re.compile("^(text)|(email)")})
-    input_password = html.find("input", {"type":"password"})
-    id_username = input_username.attrs["id"]
-    id_password = input_password.attrs["id"]
-    time.sleep(1)
-    browser.find_element_by_id(id_username).send_keys(username)
-    browser.find_element_by_id(id_password).send_keys(password+"\n")
-    time.sleep(1)
+def search_facebook():
     # html_startpage = browser.page_source
     # html = BeautifulSoup(html_startpage, "html.parser")
     # search_field = html.find("input", attrs={'type': re.compile("^(text)"), "placeholder": re.compile("^(Suche).*")})
@@ -67,9 +37,87 @@ def login_to_any_page(url):
     # search_field = browser.find_element_by_xpath(xpath_string).send_keys("Michael Fürer"+"\n")
     # browser.find_element_by_xpath("//a[@href='/"+"Michael Fürer"+"/']").click()
     # print(search_field.hq)
-    browser.get("https://www.facebook.com/search/str/Michael%20F%C3%BCrer/users-named/str/ZF%20Transmission/"
-                "pages-named/employees/present/intersect")
+    browser.get("https://www.facebook.com/search/str/Michael%20Fürer/users-named")
+    person = browser.find_elements_by_link_text("Michael Fürer")
+    print(person[0].get_attribute("href"))
+    browser.get(person[0].get_attribute("href"))
+    html_of_search = browser.page_source
+    html = BeautifulSoup(html_of_search, "html.parser")
+    time.sleep(1)
 
+    person_soup = html.find("div", id="intro_container_id")
+    print(person_soup.text)
+    #for elem in html(text=re.compile("^(Steckbrief)")):
+        #print(elem)
+
+
+def search_instagram(person):
+    search_url = "https://www.instagram.com/" + person.instagram_name + "/"
+    browser.get(search_url)
+    html_of_search = browser.page_source
+    html = BeautifulSoup(html_of_search, "html.parser")
+
+    person_soup = html.find("div",attrs={"class": "-vDIg"})
+    print(person_soup.prettify())
+
+    search = "https://www.google.com/search?q=site%3Ainstagram.com+%27" + person.instagram_name + "%27+-site%3Ainstagram.com%2F"+ \
+             person.instagram_name + "&oq=site%3Ainstagram.com+%27" + \
+             person.instagram_name +"%27+-site%3Ainstagram.com%2F"+ person.instagram_name
+    browser.get(search)
+
+def search_linkedin(person):
+    browser.get("https://www.linkedin.com/in/sebastian-frank-4608a6158/")
+    html_of_search = browser.page_source
+    html = BeautifulSoup(html_of_search, "html.parser")
+
+    person_soup = html.find("div", attrs={"class": "mt4 display-flex ember-view"})
+    #person_soup = html.find(text="Sebastian Frank")
+    print(person_soup.text)
+    stringtest = person_soup.text
+
+    print(stringtest.replace("\n", "").replace("  ",""))
+
+def get_person_information():
+    """first_name = input("Vorname: ")
+    second_name = input("Nachname: ")
+    location = input("Ort: ")
+    year_of_birth = input("Geburtsjahr: ")
+    instagram_name = input("Instagram-Name: ")
+    facebook_name = input("Facebook-Name: ")
+    company = input("Firma: ")"""
+    # person_object = Person(first_name, second_name, location, year_of_birth, instagram_name, facebook_name, company)
+
+    person_object = Person("Sebastian", "", "", "", "", "", "")
+
+    if person_object.instagram_name is not "":
+        print(person_object.instagram_name)
+        login_to_any_page(instagram_login_page)
+        search_instagram(person_object)
+
+    elif person_object.facebook_name is not "":
+        login_to_any_page(facebook_login_page)
+        search_facebook()
+    elif person_object.first_name is not "":
+        login_to_any_page(linkedin_login_page)
+        search_linkedin(person_object)
+
+def login_to_any_page(url):
+
+    browser.maximize_window()
+    browser.get(url)
+    time.sleep(1)
+    html_of_search = browser.page_source
+    html = BeautifulSoup(html_of_search, "html.parser")
+    input_username = html.find("input", attrs={'type': re.compile("^(text)|(email)")})
+    input_password = html.find("input", {"type":"password"})
+    id_username = input_username.attrs["id"]
+    id_password = input_password.attrs["id"]
+    time.sleep(1)
+    browser.find_element_by_id(id_username).send_keys(username)
+    browser.find_element_by_id(id_password).send_keys(password+"\n")
+    time.sleep(1)
 
 get_person_information()
 # login_to_any_page(facebook_login_page)
+
+
