5b7360254c7342e02f36e27cd99a1b03b11f0f46
Marco Lang <marcolang95@web.de>
2019-02-08 19:17:02 +0100

    RAKE

--- Rake.py
+++ Rake.py
@@ -9,34 +9,29 @@ german_stopwords = stopwords.words('german')
 
 
 r = Rake(language="german",stopwords=german_stopwords, ranking_metric=Metric.DEGREE_TO_FREQUENCY_RATIO,max_length=4)
-r_degree = Rake(language="german", stopwords=german_stopwords,ranking_metric=Metric.WORD_DEGREE,max_length=4)
-r_frequency = Rake(language="german", stopwords=german_stopwords,ranking_metric=Metric.WORD_FREQUENCY,max_length=4)
-
+#r_degree = Rake(language="german", stopwords=german_stopwords,ranking_metric=Metric.WORD_DEGREE,max_length=4)
+#r_frequency = Rake(language="german", stopwords=german_stopwords,ranking_metric=Metric.WORD_FREQUENCY,max_length=4)
+"""
 browser = webdriver.Chrome('/home/marco/Downloads/chromedriver')
 browser.implicitly_wait(10)
 browser.get("https://businesspf.hs-pforzheim.de/studium/studierende/bachelor/bw_einkauf_logistik/studierende/studentisches_leben/")
 html_of_search = browser.page_source
 html = BeautifulSoup(html_of_search, "html.parser")
 html_string = str(html.body.text)
+"""
+text = "In einer jungen Wissenschaft wie der Informatik mit ihrer Vielschichtigkeit und ihrer unüberschaubaren " \
+       "Anwendungsvielfalt ist man oftmals noch bestrebt, eine Charakterisierung des Wesens dieser Wissenschaft" \
+       " und Gemeinsamkeiten und Abgrenzungen zu anderen Wissenschaften zu finden. Etablierte Wissenschaften haben" \
+       " es da leichter, sei es, dass sie es aufgegeben haben, sich zu definieren, oder sei es, dass ihre Struktur " \
+       "und ihre Inhalte allgemein bekannt sind."
 
-text = "Auch wenn Sie sich nicht aktiv engagieren wollen, können Sie an den regelmäßigen Exkursionen der Studentengruppe" \
-       " teilnehmen und Unternehmen kennen lernen, die gerne Praktikanten und Absolventen aus Pforzheim als Kollegen begrüßen. " \
-       "Über anstehende Exkursionen informieren wir Sie auf unserer News-Seite. Und wenn Sie einfach nur mal einen geselligen Abend" \
-       " mit vielen Gleichgesinnten verbringen wollen? Prima! Dann kommen Sie zum Logistiker-Stammtisch, der einmal im Semester stattfindet." \
-       " Infos auf unserer News-Seite. Seit dem 28.03.2018 gibt es eine neue Hochschulgruppe im Studiengang Einkauf und Logistik. " \
-       "Die Idee zu Logistik Live, kurz LogLive, entstand Mitte 2017, nachdem seit 2015 aufgrund fehlender finanzieller " \
-       "Mittel keine Exkursionen mehr stattfinden konnten. Hier möchten wir etwas ändern. Jedes Semester sollen künftig bis" \
-       " zu zwei Exkursionen zu Firmen in der näheren Umgebung organisiert werden, also im Großraum Karlsruhe, Pforzheim " \
-       "und Stuttgart. Das Angebot richtet sich vor allem an Studentinnen und Studenten des 3. bis 5. Semesters, generell" \
-       " ist aber jeder willkommen. Bei Fragen oder Anregungen oder Interesse an der Mitarbeit könnt Ihr gerne die beiden " \
-       "BEL-Studenten und Initiatoren Nicole Schneider und Michael Fürer ansprechen. Wir freuen uns auf spannende Exkursionen " \
-       "mit Euch. "
-
-r.extract_keywords_from_text(html_string)
-r_degree.extract_keywords_from_text(html_string)
-r_frequency.extract_keywords_from_text(html_string)
-
+r.extract_keywords_from_text(text)
 
 print(r.get_ranked_phrases())
-print(r_degree.get_ranked_phrases())
-print(r_frequency.get_ranked_phrases())
+print(r.get_ranked_phrases_with_scores())
+print(r.get_word_degrees())
+print(r.get_word_frequency_distribution())
+
+
+#print(r_degree.get_ranked_phrases())
+#print(r_frequency.get_ranked_phrases())
