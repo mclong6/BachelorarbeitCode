2e6167472048c9d45714fe55dc0a72f953303ad3
Marco Lang <marcolang95@web.de>
2019-03-10 19:19:10 +0100

    Scrapy_Selenium, Class Creat_Link_List

--- Personensuche_Bachelorarbeit.py
+++ Personensuche_Bachelorarbeit.py
@@ -1,120 +1,121 @@
 
+class Create_Search_Link:
+    class Person(object):
+        def __init__(self):
+            self.first_name = ""
+            self.second_name = ""
+            self.location = ""
+            self.year_of_birth = ""
+            self.estimated_year_of_birth = ""
+            self.instagram_name = ""
+            self.facebook_name = ""
+            self.twitter_name = ""
+            self.institution = ""
 
-instagram_key = 1
-facebook_key = 2
-twitter_key = 3
-institution_key = 4
-location_key = 5
-year_of_birth_key = 6
-location_year_key = 7
-location_institution_key = 8
-location_year_institution_key = 9
-
-
-search_link_list = []
-
-class Person(object):
-    def __init__(self):
-        self.first_name = ""
-        self.second_name = ""
-        self.location = ""
-        self.year_of_birth = ""
-        self.estimated_year_of_birth = ""
-        self.instagram_name = ""
-        self.facebook_name = ""
-        self.twitter_name = ""
-        self.institution = ""
-
-
-def enter_information():
-    person_object = Person()
-    person_object.first_name = input("Vorname: ").replace(" ","%22")
-    person_object.second_name = input("Nachname: ").replace(" ","%22")
-    person_object.location = input("Wohnort: ").replace(" ","%22")
-    person_object.year_of_birth = input("Genaues Geburtsjahr: ").replace(" ","%22")
-    person_object.estimated_year_of_birth = input("Geschätztes Geburtsjahr: ").replace(" ","%22")
-    person_object.institution = input("Institution: ").replace(" ","%22")
-    #person_object.instagram_name = input("Instagram Benutzername: ")
-    #person_object.facebook_name = input("Facebook Benutzername: ")
-    #person_object.twitter_name = input("Twitter Benutzername")
-
-    return person_object
+    def __init__(self, instagram_key = 1, facebook_key = 2, twitter_key = 3, institution_key = 4, location_key = 5,
+                 year_of_birth_key = 6,location_year_key = 7,location_institution_key = 8,
+                 location_year_institution_key = 9, search_link_list = []):
+        self.instagram_key=instagram_key
+        self.facebook_key=facebook_key
+        self.twitter_key=twitter_key
+        self.institution_key=institution_key
+        self.location_key=location_key
+        self.year_of_birth_key=year_of_birth_key
+        self.location_year_key=location_year_key
+        self.location_institution_key=location_institution_key
+        self.location_year_institution_key=location_year_institution_key
+        self.search_link_list=search_link_list
+        self.person_object = self.Person()
 
 
-def analyze_information(person_object):
-    if person_object.instagram_name is not "":
-        print("Instagram name is not none")
-        build_search_string(person_object,instagram_key)
-    if person_object.facebook_name is not "":
-        print("Facebook name is not none")
-        build_search_string(person_object,facebook_key)
-    if person_object.twitter_name is not "":
-        print("Twitter name is not none")
-        build_search_string(twitter_key)
-    if person_object.first_name and person_object.second_name is not "":
-        if person_object.location is not "":
-            build_search_string(person_object, location_key)
-            if person_object.year_of_birth is not "":
-                build_search_string(person_object,location_year_key)
-                if person_object.institution is not "":
-                    build_search_string(person_object, location_year_institution_key)
-            if person_object.institution is not "":
-                build_search_string(person_object,location_institution_key)
-        if person_object.year_of_birth is not "":
-            build_search_string(person_object, year_of_birth_key)
-        if person_object.institution is not "":
-            build_search_string(person_object, institution_key)
-    #location kann gespeichert werden damit auf einer Webseite danach gesucht werden kann. Ode zu städte liste hinzufügen
+    def enter_information(self):
+        self.person_object.first_name = input("Vorname: ").replace(" ","%22")
+        self.person_object.second_name = input("Nachname: ").replace(" ","%22")
+        self.person_object.location = input("Wohnort: ").replace(" ","%22")
+        self.person_object.year_of_birth = input("Genaues Geburtsjahr: ").replace(" ","%22")
+        self.person_object.estimated_year_of_birth = input("Geschätztes Geburtsjahr: ").replace(" ","%22")
+        self.person_object.institution = input("Institution: ").replace(" ","%22")
+        #self.person_object.instagram_name = input("Instagram Benutzername: ")
+        #self.person_object.facebook_name = input("Facebook Benutzername: ")
+        #self.person_object.twitter_name = input("Twitter Benutzername")
+        self.analyze_information()
+
+    def analyze_information(self):
+
+        if self.person_object.instagram_name is not "":
+            print("Instagram name is not none")
+            self.build_search_string(self.instagram_key)
+        if self.person_object.facebook_name is not "":
+            print("Facebook name is not none")
+            self.build_search_string(self.facebook_key)
+        if self.person_object.twitter_name is not "":
+            print("Twitter name is not none")
+            self.build_search_string(self.twitter_key)
+        if self.person_object.first_name and self.person_object.second_name is not "":
+            if self.person_object.location is not "":
+                self.build_search_string(self.location_key)
+                if self.person_object.year_of_birth is not "":
+                    self.build_search_string(self.location_year_key)
+                    if self.person_object.institution is not "":
+                        self.build_search_string(self.location_year_institution_key)
+                if self.person_object.institution is not "":
+                    self.build_search_string(self.location_institution_key)
+            if self.person_object.year_of_birth is not "":
+                self.build_search_string(self.year_of_birth_key)
+            if self.person_object.institution is not "":
+                self.build_search_string(self.institution_key)
+        #location kann gespeichert werden damit auf einer Webseite danach gesucht werden kann. Ode zu städte liste hinzufügen
 
 
-def build_search_string(person_object, key):
-    if key == facebook_key:
-        search_string = ""
-        search_link_list.append(search_string)
+    def build_search_string(self, key):
+        if key == self.facebook_key:
+            search_string = ""
+            self.search_link_list.append(search_string)
 
-    if key == instagram_key:
-        search_string = "https://www.instagram.com/" + person_object.instagram_name + "/"
-        search_link_list.append(search_string)
+        if key == self.instagram_key:
+            search_string = "https://www.instagram.com/" + self.person_object.instagram_name + "/"
+            self.search_link_list.append(search_string)
 
-    if key == twitter_key:
-        search_string = "https://twitter.com/search?f=users&q=%20" + person_object.first_name + "%20" + \
-                        person_object.second_name
-        search_link_list.append(search_string)
+        if key == self.twitter_key:
+            search_string = "https://twitter.com/search?f=users&q=%20" + self.person_object.first_name + "%20" + \
+                            self.person_object.second_name
+            self.search_link_list.append(search_string)
 
-    if key == location_key:
-        search_string = "https://www.google.com/search?q=%22" + person_object.first_name + "+" \
-                        + person_object.second_name + "%22+" + "%22" + person_object.location + "%22"
-        search_link_list.append(search_string)
+        if key == self.location_key:
+            search_string = "https://www.google.com/search?q=%22" + self.person_object.first_name + "+" \
+                            + self.person_object.second_name + "%22+" + "%22" + self.person_object.location + "%22"
+            self.search_link_list.append(search_string)
 
-    if key == year_of_birth_key:
-        search_string = "https://www.google.com/search?q=%22" + person_object.first_name + "+" \
-                        + person_object.second_name + "%22+" + "%22"+person_object.year_of_birth + "%22"
-        search_link_list.append(search_string)
+        if key == self.year_of_birth_key:
+            search_string = "https://www.google.com/search?q=%22" + self.person_object.first_name + "+" \
+                            + self.person_object.second_name + "%22+" + "%22"+ self.person_object.year_of_birth + "%22"
+            self.search_link_list.append(search_string)
 
-    if key == institution_key:
-        search_string = "https://www.google.com/search?q=%22" + person_object.first_name + "+" \
-                        + person_object.second_name + "%22+" + "%22"+person_object.institution + "%22"
-        search_link_list.append(search_string)
+        if key == self.institution_key:
+            search_string = "https://www.google.com/search?q=%22" + self.person_object.first_name + "+" \
+                            + self.person_object.second_name + "%22+" + "%22"+self.person_object.institution + "%22"
+            self.search_link_list.append(search_string)
 
-    if key == location_year_key:
-        search_string = "https://www.google.com/search?q=%22" + person_object.first_name + "+" \
-                        + person_object.second_name + "%22+" + "%22"+person_object.location + "%22+" + "%22" \
-                        + person_object.year_of_birth + "%22"
-        search_link_list.append(search_string)
+        if key == self.location_year_key:
+            search_string = "https://www.google.com/search?q=%22" + self.person_object.first_name + "+" \
+                            + self.person_object.second_name + "%22+" + "%22"+self.person_object.location + "%22+" + "%22" \
+                            + self.person_object.year_of_birth + "%22"
+            self.search_link_list.append(search_string)
 
-    if key == location_institution_key:
-        search_string = "https://www.google.com/search?q=%22" + person_object.first_name + "+" \
-                        + person_object.second_name + "%22+" + "%22" + person_object.location + "%22+" + "%22" \
-                        + person_object.institution + "%22"
-        search_link_list.append(search_string)
+        if key == self.location_institution_key:
+            search_string = "https://www.google.com/search?q=%22" + self.person_object.first_name + "+" \
+                            + self.person_object.second_name + "%22+" + "%22" + self.person_object.location + "%22+" + "%22" \
+                            + self.person_object.institution + "%22"
+            self.search_link_list.append(search_string)
 
-    if key == location_year_institution_key:
-        search_string = "https://www.google.com/search?q=%22" + person_object.first_name + "+" \
-                    + person_object.second_name + "%22+" + "%22" + person_object.location + "%22+" + "%22" \
-                    + person_object.year_of_birth + "%22+" + "%22" + person_object.institution + "%22"
-        search_link_list.append(search_string)
+        if key == self.location_year_institution_key:
+            search_string = "https://www.google.com/search?q=%22" + self.person_object.first_name + "+" \
+                        + self.person_object.second_name + "%22+" + "%22" + self.person_object.location + "%22+" + "%22" \
+                        + self.person_object.year_of_birth + "%22+" + "%22" + self.person_object.institution + "%22"
+            self.search_link_list.append(search_string)
+        return self.search_link_list
 
-
+"""
 person_object = enter_information()
 analyze_information(person_object)
-print(search_link_list)
+print(search_link_list)"""
--- Scrapy_Selenium.py
+++ Scrapy_Selenium.py
@@ -2,7 +2,8 @@ from scrapy_selenium import SeleniumRequest
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from shutil import which
-
+from bs4 import BeautifulSoup
+import Personensuche_Bachelorarbeit
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
@@ -12,28 +13,24 @@ class QuotesSpider(scrapy.Spider):
         'SELENIUM_DRIVER_ARGUMENTS' : ['--headless'],  # '--headless' if using chrome instead of firefox
         'DOWNLOADER_MIDDLEWARES' : {
         'scrapy_selenium.SeleniumMiddleware': 800
+        }
     }
-    }
-    """
-    SELENIUM_DRIVER_NAME = 'chrome'
-    SELENIUM_DRIVER_EXECUTABLE_PATH = which('/home/marco/Downloads/chromedriver')
-    SELENIUM_DRIVER_ARGUMENTS = ['--headless']  # '--headless' if using chrome instead of firefox
-    DOWNLOADER_MIDDLEWARES = {
-        'scrapy_selenium.SeleniumMiddleware': 800
-    }"""
     def start_requests(self):
-        urls = [
-            'https://www.instagram.com/lamarcong',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            print("test "+url)
-            #yield scrapy.Request(url=url, callback=self.parse)
-            #yield SeleniumRequest(url=url, callback=self.parse_result)
-            yield SeleniumRequest(url=url, callback=self.parse_result)
+        Create_Search_Link=Personensuche_Bachelorarbeit.Create_Search_Link()
+        url_list = Create_Search_Link.enter_information()
+        print(url_list)
+        urls = ['http://quotes.toscrape.com/page/2/', 'https://www.instagram.com/lamarcong']
 
-    def parse_result(self, response):
-        print(response.request.meta['driver'].page_source)
+        for url in urls:
+            print("URL:  "+url)
+            yield SeleniumRequest(url=url, callback=self.parse, wait_time=10)
+            #yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        #print(response.request.meta['driver'].page_source)
+        obj = BeautifulSoup(response.text, "html.parser")
+        obj
+        print(obj)
 
 process = CrawlerProcess({
 "user-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0"
